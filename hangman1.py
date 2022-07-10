2e49ecc9242f5ba625f294fd3529a30f3bae9695
Sumit <kumara4749@gmail.com>
2022-07-10 20:32:12 +0530

    commit

--- hangman1.py
+++ hangman1.py
@@ -1,7 +1,6 @@
 #Step 1 
 
 word_list = ["aardvark", "baboon", "camel"]
-
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.py
 import random
 chosen_word=random.choice(word_list)
