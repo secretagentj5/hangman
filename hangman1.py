5592c68647728911aab4063b1d5b0b73428a8e1c
Sumit <kumara4749@gmail.com>
2022-07-10 20:32:58 +0530

    commit

--- hangman1.py
+++ hangman1.py
@@ -6,7 +6,6 @@ import random
 chosen_word=random.choice(word_list)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess=input("guess a letter: ").lower()
-
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 for letter in chosen_word:
     if letter == guess:
