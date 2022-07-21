a7fb10ffd52eb6382e54ef2e386052e9c7d22192
sumit kumar <kumara4749@gmail.com>
2022-07-21 21:56:48 +0530

    commit

--- hangman2.py
+++ hangman2.py
@@ -17,7 +17,6 @@ for _ in range(word_length):
     display+="_"
 print(display)
 guess = input("Guess a letter: ").lower()
-
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
