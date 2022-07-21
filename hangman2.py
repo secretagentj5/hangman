ea402f2f5ef12eeae2d357df5fb3f776189873ed
sumit kumar <kumara4749@gmail.com>
2022-07-21 21:56:56 +0530

    commit

--- hangman2.py
+++ hangman2.py
@@ -24,7 +24,6 @@ for position in range(word_length):
     letter = chosen_word[position]
     if letter==guess:
         display[position]=letter
-
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
 print(display)
