f21f9a17e7d4327628ef50ede427daf785b8a2ab
sumit kumar <kumara4749@gmail.com>
2022-07-21 21:56:03 +0530

    commit

--- hangman2.py
+++ hangman2.py
@@ -7,7 +7,6 @@ chosen_word = random.choice(word_list)
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
-
 #TODO-1: - Create an empty List called display.
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
